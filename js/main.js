e2021c418787b2a88d3993404d1d479b4f6b331d
thomp <thompsondjodjo021@gmail.com>
2025-02-22 20:42:53 +0100

    Test1

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const messageBar = document.querySelector(".bar-wrapper input");
+const sendBtn = document.querySelector(".bar-wrapper button");
+const messageBox = document.querySelector(".message-box");
+
